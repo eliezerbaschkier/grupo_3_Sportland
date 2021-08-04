2728cc49babfe09ea7ead285ca35b1017ddfab9e
Eliezer Baschkier <eliezerbaschkier@gmail.com>
2021-08-04 10:53:24 -0300

    Basic server setup in app.js, and creation of html files

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './')
-console.log(publicPath);
 
-//app.listen()
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+})
+
+app.get('/', (req,res) => {
+    let pathHome = path.resolve(publicPath, './views/index.html');
+    res.sendFile(pathHome);
+})
