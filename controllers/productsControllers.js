6ddb19b331a5d0a16155e37feef674b993361518
Eliezer Baschkier <eliezerbaschkier@gmail.com>
2021-08-22 17:50:42 -0300

    Basic setup and routing of createProduct and editProduct

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -9,6 +9,16 @@ const productsControllers = {
     productCart: (req,res) => {
         let title = 'Tu carrito';
         res.render('./products/productCart', {title: title});
+    },
+
+    createProduct: (req,res) => {
+        let title = 'Crear producto';
+        res.render('./products/createProduct', {title: title});
+    },
+    
+    editProduct: (req,res) => {
+        let title = 'Editar producto';
+        res.render('./products/editProduct', {title: title});
     }
 };
 
--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,8 @@ const productsControllers = require('../controllers/productsControllers')
 
 router.get('/productDetail', productsControllers.productDetail);
 router.get('/productCart', productsControllers.productCart);
+router.get('/createProduct', productsControllers.createProduct);
+router.get('/editProduct', productsControllers.editProduct);
 router.post('/productDetail', productsControllers.productDetail);
 router.post('/productCart', productsControllers.productCart);
 
