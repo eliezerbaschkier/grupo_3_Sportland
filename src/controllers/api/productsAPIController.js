b692c6e12560f921d8c35763d6bbe0e4d9ed65bd
Eliezer Baschkier <eliezerbaschkier@gmail.com>
2021-12-08 16:53:12 -0300

    Changes to API productsList and created productDetail API

--- src/controllers/api/productsAPIController.js
+++ src/controllers/api/productsAPIController.js
@@ -5,17 +5,91 @@ const { Op } = require("sequelize");
 const productsAPIControllers = {
     productList: (req,res) => {
         let title = 'API Products List';
-        db.Product.findAll()
-            .then(products => {
+        let productsRequest = db.Product.findAll({
+            attributes: ['id', 'name', 'description'],
+            include: {
+                association: 'categories'
+                /*
+                model: 'productCategory',
+                as: 'category',
+                attributes: ['name'],
+                through: {
+                    attributes: []
+                }
+                */
+            }
+        });
+        let categoriesRequest = db.ProductCategory.findAll({
+            attributes: ['id', 'name']
+        });
+
+        Promise.all([productsRequest, categoriesRequest])
+            .then(([products, categories]) => {
+                //let categoriesList = [];
+                /*
+                for (i = 0 ; i < categories.length ; i++) {
+                    categoriesList.push({
+                        id: categories[i]['dataValues']['id'],
+                        name: categories[i]['dataValues']['name']
+                    })
+                };
+                */
+                for(i = 0 ; i < products.length ; i++) {
+                    products[i]['dataValues']['detail'] = 'api/products/' + products[i].id;
+                };
+                
+                for(i = 0 ; i < products.length ; i++) {
+                    delete products[i].dataValues.categories.dataValues.id;
+                    delete products[i].dataValues.categories.dataValues.created_at;
+                    delete products[i].dataValues.categories.dataValues.modified_at;
+                    delete products[i].dataValues.categories.dataValues.deleted_at;
+                }
+                
+
                 let respuesta = {
                     meta: {
                         status : 200,
-                        total: products.length,
                         url: 'api/products'
                     },
-                    data: products
-                }
-                    res.json(respuesta);
+                    count: products.length,
+                    // countByCategory: {}
+                    products,
+                };
+                
+                res.json(respuesta);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+
+    productDetail: (req,res) => {
+        let title = 'API product detail';
+        db.Product.findByPk(req.params.id,{
+            attributes: ['id', 'name', 'description', 'price', 'image'],
+            include: {
+                association: 'categories'
+            }
+        })
+            .then(product => {
+                delete product.dataValues.categories.dataValues.id;
+                delete product.dataValues.categories.dataValues.created_at;
+                delete product.dataValues.categories.dataValues.modified_at;
+                delete product.dataValues.categories.dataValues.deleted_at;
+
+                console.log(product.dataValues.image);
+                product.dataValues.image = '/images/products/' + product.dataValues.image;
+
+
+                let respuesta = {
+                    meta: {
+                        status : 200,
+                        total: product.length,
+                        url: 'api/products/' + product.dataValues.id
+                    },
+                    data: product
+                };
+                res.json(respuesta);            
             })
             .catch((error) => {
                 console.log(error);
--- src/routes/api/products.js
+++ src/routes/api/products.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productsAPIControllers = require('../../controllers/api/productsAPIController');
 
 router.get('/', productsAPIControllers.productList);
+router.get('/:id', productsAPIControllers.productDetail);
 
 module.exports = router;
